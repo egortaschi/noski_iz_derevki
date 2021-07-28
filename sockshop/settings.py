3ac17b74a8c1e3538e8b02f3ada09cb658ab413f
egortaschi <etaschilov17@gmail.com>
2021-07-28 21:09:34 +0300

    settings.py modified(dotenv)

--- sockshop/settings.py
+++ sockshop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^34rh=g1d7hj8l5j_&d3x-$q6auy%lne1)%#pf(i0j)!a5gs#n'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
